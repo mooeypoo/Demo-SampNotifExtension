016e649a5b086c008b1d8631c712fe3178d2c4bd
mooeypoo <moriel@gmail.com>
2024-10-26 23:55:06 -0400

    Adding triggers
    Adding some attempted logic of where complexity should go in terms
    of the presentation logic with Triggers (hooks) for the notifications.

---
+++ includes/Triggers.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ * This file holds the triggers for the notifications this extension defines.
+ * It would usually be named "Hooks" since it uses hooks to trigger the notifications,
+ * but for the sake of this demo and clarity of operation, I'm calling it "Triggers".
+ */
+
+namespace MediaWiki\Extension\SampNotifExtension;
+
+use MediaWiki\Hook\PageSaveComplete;
+use MediaWiki\Hook\LocalUserCreated;
+use MediaWiki\Hook\LinksUpdateComplete;
+use MediaWiki\Extension\Notifications\EchoEvent;
+
+class Hooks implements
+	PageSaveComplete,
+	LocalUserCreated,
+	LinksUpdateComplete
+{
+	public function onPageSaveComplete(
+		$wikiPage,
+		$userIdentity,
+		$summary,
+		$flags,
+		$revisionRecord,
+		$editResult
+	) {
+		if ( $editResult->isNullEdit() ) {
+			return;
+		}
+
+		// [ ... logic ... ]
+
+		// If the user is not an IP and this is not a null edit,
+		// test for them reaching a congratulatory threshold
+		$thresholds = [ 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000 ];
+		if ( $userIdentity->isRegistered() ) {
+			$thresholdCount = $this->getEditCount( $userIdentity );
+			if ( in_array( $thresholdCount, $thresholds ) ) {
+				DeferredUpdates::addCallableUpdate( static function () use (
+					$revisionRecord, $userIdentity, $title, $thresholdCount
+				) {
+					// [ ... logic to prevent thanking the user more than once ... ]
+
+					// This used to be in the presentation model, but it should be
+					// used to know how to create the requested notification:
+					if ( !$this->event->getTitle() ) {
+						return false;
+					}
+			
+					if ( $this->event->getExtraParam( 'revid' ) ) {
+						$params = [
+							'oldid' => 'prev',
+							'diff' => $this->event->getExtraParam( 'revid' )
+						];
+					} else {
+						$params = [];
+					}
+					$url = $this->event->getTitle()->getLocalURL( $params );
+			
+					Notification::create( [
+						'type' => 'thank-you-edit',
+						'title' => $title,
+						'agent' => $userIdentity,
+						// Edit threshold notifications are sent to the agent
+						'extra' => [
+							'editCount' => $thresholdCount,
+							'revid' => $revisionRecord->getId(),
+						],
+						'presentation' => [
+							// Overriding (or augmenting) the default definition so we can give the correct
+							// i18n key
+							'body' => [
+								'msg' => 'notification-header-thank-you-' + $thresholdCount + '-edit',
+								'params' => [ $userIdentity ]
+							],
+							'links' => [
+								[
+									'url' => $url,
+									'label' => $this->msg( 'notification-link-thank-you-edit', $this->getViewingUserForGender() )->text()
+								]
+							]
+						]
+					] );
+				} );
+			}
+		}
+
+	}
+
+	public function onLocalUserCreated( $user, $autocreated ) {
+		if ( !$autocreated ) {
+
+			// Moved from Presentation Model; the logic should be
+			// where the code calls the creation of the notification
+			$primaryLink = []
+			$msg = $this->msg( 'notification-welcome-link' );
+
+			if ( !$msg->isDisabled() && $title ) {
+				$title = Title::newFromText( $msg->plain() );
+				$primaryLink = [
+					'url' => $title->getFullURL(),
+					'label' => $this->msg( 'notification-welcome-linktext' )->text(),
+				];
+			}
+
+			Notification::create( [
+				'type' => 'welcome',
+				'agent' => $user,
+				'presentation' => [
+					'links' => [
+						$primaryLink
+					]
+				]
+			] );
+		}
+	}
+
+	public function onLinksUpdateComplete( $linksUpdate, $ticket ) {
+		// Rollback or undo should not trigger link notification
+		if ( $linksUpdate->getRevisionRecord() ) {
+			$revId = $linksUpdate->getRevisionRecord()->getId();
+			if ( isset( self::$revertedRevIds[$revId] ) ) {
+				return;
+			}
+		}
+
+		// Handle only
+		// 1. content namespace pages &&
+		// 2. non-transcluding pages &&
+		// 3. non-redirect pages
+		if ( !$this->namespaceInfo->isContent( $linksUpdate->getTitle()->getNamespace() )
+			|| !$linksUpdate->isRecursive() || $linksUpdate->getTitle()->isRedirect()
+		) {
+			return;
+		}
+
+		$revRecord = $linksUpdate->getRevisionRecord();
+		$revid = $revRecord ? $revRecord->getId() : null;
+		$user = $revRecord ? $revRecord->getUser() : null;
+
+		// link notification is boundless as you can include infinite number of links in a page
+		// db insert is expensive, limit it to a reasonable amount, we can increase this limit
+		// once the storage is on Redis
+		$max = 10;
+		// Only create notifications for links to content namespace pages
+		// @Todo - use one big insert instead of individual insert inside foreach loop
+		foreach ( $linksUpdate->getPageReferenceIterator( 'pagelinks', LinksTable::INSERTED ) as $pageReference ) {
+			if ( $this->namespaceInfo->isContent( $pageReference->getNamespace() ) ) {
+				$title = Title::newFromPageReference( $pageReference );
+				if ( $title->isRedirect() ) {
+					continue;
+				}
+
+				$linkFromPageId = $linksUpdate->getTitle()->getArticleID();
+				// T318523: Don't send page-linked notifications for pages created by bot users.
+				$articleAuthor = UserLocator::getArticleAuthorByArticleId( $title->getArticleID() );
+				if ( $articleAuthor && $articleAuthor->isBot() ) {
+					continue;
+				}
+
+				$bundleString = $event->getType();
+				if ( $event->getTitle() ) {
+					$bundleString .= '-' . $event->getTitle()->getNamespace()
+						. '-' . $event->getTitle()->getDBkey();
+				}
+
+				Notification::create( [
+					'type' => 'page-linked',
+					'title' => $title,
+					'agent' => $user,
+					'extra' => [
+						'target-page' => $linkFromPageId,
+						'link-from-page-id' => $linkFromPageId,
+						'revid' => $revid,
+					],
+					// Bundle-id is created here, since there are rules
+					// on how to bundle notifications of this type based
+					// on the page title, rather than all notifications
+					// of this type generally
+					"bundle-id" => $bundleString
+				] );
+				$max--;
+			}
+			if ( $max < 0 ) {
+				break;
+			}
+		}
+	}
+
+}
